3ff0441260fe7fe79d13e0a7cb22597b31c95423
root <root@localhost.localdomain>
2023-11-29 11:59:29 +0800

    add packages

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-docker run -it -v `pwd`/App:/App -w /App --rm mcr.microsoft.com/dotnet/sdk:8.0 dotnet build
+docker run -it -v `pwd`/App:/App -v `pwd`/packages:/root/.nuget/packages -w /App --rm mcr.microsoft.com/dotnet/sdk:8.0 dotnet build
