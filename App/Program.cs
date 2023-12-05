87f930855df510ea4120c016c23271ff7621f493
huang <495031718@qq.com>
2023-12-05 10:09:08 +0800

    attributes

--- App/Program.cs
+++ App/Program.cs
@@ -5,41 +5,89 @@ class App {
     static void Main(string[] args)
     {
         Mxfile mxfile = new(){
-            diagrams = new[]{
-                new Diagram() {
-                    mxGraphModels = new[]{
-                        new MxGraphModel() {
-                            roots = new[]{
-                                new Root(){
-                                    mxcells = new []{
-                                        new Mxcell(){
+            diagrams = new Diagram[]{
+                new() {
+                    mxGraphModels = new MxGraphModel[]{
+                        new() {
+                            roots = new Root[]{
+                                new(){
+                                    mxcells = new Mxcell[]{
+                                        new(){
                                             id = "0000",
-                                            parent = "0"
+                                            parent = "0",
+                                            _value = "value",
+                                            style = "style",
+                                            vertex = "vertex",
+                                            mxGeometry = new MxGeometry(){
+                                                x = "0",
+                                                y = "0",
+                                                width = "0",
+                                                height = "0",
+                                                _as = "0",
+                                                relative = "1",
+                                                mxPoint = new MxPoint[]{
+                                                    new(){_as="target"},
+                                                    new(){_as="source"}
+                                                }
+                                            }
                                         },
                                         new(){
                                             id = "0001",
-                                            parent = "0"
-                                        },
+                                            parent = "0",
+                                            _value = "value",
+                                            style = "style",
+                                            vertex = "vertex",
+                                            mxGeometry = new MxGeometry(){
+                                                x = "0",
+                                                y = "0",
+                                                width = "0",
+                                                height = "0",
+                                                _as = "0",
+                                                relative = "0",
+                                            }
+                                        }
                                     }
                                 }
                             }
                         }
                     }
                 },
-                new Diagram() {
-                    mxGraphModels = new[]{
-                        new MxGraphModel() {
-                            roots = new[]{
-                                new Root(){
-                                    mxcells = new []{
-                                        new Mxcell(){
+                new() {
+                    mxGraphModels = new MxGraphModel[]{
+                        new() {
+                            roots = new Root[]{
+                                new(){
+                                    mxcells = new Mxcell[]{
+                                        new(){
                                             id = "1000",
-                                            parent = "1"
+                                            parent = "1",
+                                            _value = "value",
+                                            style = "style",
+                                            vertex = "vertex",
+                                            mxGeometry = new MxGeometry(){
+                                                x = "0",
+                                                y = "0",
+                                                width = "0",
+                                                height = "0",
+                                                _as = "0",
+                                                relative = "0",
+                                            }
                                         },
                                         new(){
                                             id = "1001",
-                                            parent = "1"
-                                        },
+                                            parent = "1",
+                                            _value = "value",
+                                            style = "style",
+                                            vertex = "vertex",
+                                            mxGeometry = new MxGeometry(){
+                                                x = "0",
+                                                y = "0",
+                                                width = "0",
+                                                height = "0",
+                                                _as = "0",
+                                                relative = "0",
+                                            }
+                                        }
                                     }
                                 }
                             }
--- App/models/Mxfile.cs
+++ App/models/Mxfile.cs
@@ -2,33 +2,68 @@ using System.Xml.Serialization;
 
 namespace MyModels
 {
+  [XmlRoot("mxfile")]
   public class Mxfile
   {
-    [XmlElement("Diagram")]
+    [XmlElement("diagram")]
     public Diagram[]? diagrams { get; set; }
   }
 
   public class Diagram
   {
-    [XmlElement("MxGraphModel")]
+    [XmlElement("mxGraphModel")]
     public MxGraphModel[]? mxGraphModels { get; set; }
   }
 
   public class MxGraphModel
   {
-    [XmlElement("Root")]
+    [XmlElement("root")]
     public Root[]? roots { get; set; }
   }
 
   public class Root
   {
-    [XmlElement("Mxcell")]
+    [XmlElement("mxcell")]
     public Mxcell[]? mxcells { get; set; }
   }
 
   public class Mxcell
   {
-    public string? id { get; set;}
+    [XmlAttribute("id")]
+    public string? id { get; set; }
+    [XmlAttribute("parent")]
     public string? parent { get; set; }
+    [XmlAttribute("value")]
+    public string? _value { get; set; }
+    [XmlAttribute("style")]
+    public string? style { get; set; }
+    [XmlAttribute("vertex")]
+    public string? vertex { get; set; }
+    [XmlElement("mxGeometry")]
+    public MxGeometry? mxGeometry { get; set; }
+  }
+  
+  public class MxGeometry
+  {
+    [XmlAttribute("x")]
+    public string? x { get; set; }
+    [XmlAttribute("y")]
+    public string? y { get; set; }
+    [XmlAttribute("width")]
+    public string? width { get; set; }
+    [XmlAttribute("height")]
+    public string? height { get; set; }
+    [XmlAttribute("as")]
+    public string? _as { get; set; }
+    [XmlAttribute("relative")]
+    public string? relative { get; set; }
+    [XmlElement("mxPoint")]
+    public MxPoint[]? mxPoint { get; set; }
+  }
+  
+  public class MxPoint
+  {
+    [XmlAttribute("as")]
+    public string? _as { get; set; }
   }
 }
